c910b39feff780f7ae0d7cddce5b64d481b99220
whsv26 <whsv26@gmail.com>
2021-07-01 20:29:24 +0300

    Select only latest currency rates.

--- src/main/scala/org/whsv26/crate/CurrencyRateRepository.scala
+++ src/main/scala/org/whsv26/crate/CurrencyRateRepository.scala
@@ -2,6 +2,7 @@ package org.whsv26.crate
 
 import cats.data.NonEmptyList
 import cats.effect.Sync
+import cats.implicits.toFoldableOps
 import doobie._
 import doobie.implicits._
 import doobie.util.update.Update
@@ -16,12 +17,21 @@ object CurrencyRateRepository {
 
   def impl[F[_]: Sync](xa: Transactor.Aux[F, Unit]): CurrencyRateRepository[F] = new CurrencyRateRepository[F]{
     def findByCurrencies(cs: NonEmptyList[Currency]): F[List[CurrencyRate]] = {
-      val q = fr"""
-          SELECT currency, rate, actual_at
-          FROM currency_rates
-          WHERE""" ++ Fragments.in(fr"currency", cs.map(_.toString))
+      val currValuesFr = cs.map(_.toString).map(n => fr"($n)").intercalate(fr",")
+      val cteFr = fr"WITH currencies(currency) AS (VALUES " ++ currValuesFr ++ fr")"
+      val queryFr = cteFr ++ fr"""
+          SELECT c.currency, r.rate, r.actual_at
+          FROM currencies c
+          INNER JOIN LATERAL (
+            SELECT currency, rate, actual_at
+            FROM currency_rates cr
+            WHERE cr.currency = c.currency
+            ORDER BY cr.actual_at DESC
+            LIMIT 1
+          ) r ON TRUE
+      """
 
-      q.query[CurrencyRate]
+      queryFr.query[CurrencyRate]
         .stream
         .compile
         .toList
