9eea4835e40f318e47bdd6937c4e6de8b2f35003
whsv26 <whsv26@gmail.com>
2021-07-02 00:20:14 +0300

    Fix toTimeStampString extension method timezone.

--- src/main/scala/org/whsv26/crate/Syntax.scala
+++ src/main/scala/org/whsv26/crate/Syntax.scala
@@ -1,13 +1,12 @@
 package org.whsv26.crate
 
-import java.sql.Timestamp
-import java.time.{Instant, LocalDateTime, ZoneId}
+import java.time.{Instant, LocalDateTime, ZoneId, ZoneOffset}
 
 object Syntax {
   implicit class StringSyntax(val s: String) {
     def toInstant: Instant = Instant.from(LocalDateTime.parse(s.replace(' ', 'T')).atZone(ZoneId.of("Z")))
   }
   implicit class InstantSyntax(val i: Instant) {
-    def toTimeStampString: String = Timestamp.from(i).toString
+    def toTimeStampString: String = i.atOffset(ZoneOffset.of("Z")).toLocalDateTime.toString.replace('T', ' ')
   }
 }
