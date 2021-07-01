1a59e3a963becac0a693a5374330cd3e88e61d35
whsv26 <whsv26@gmail.com>
2021-07-01 20:39:12 +0300

    Fix timezone problem.

--- src/main/scala/org/whsv26/crate/Syntax.scala
+++ src/main/scala/org/whsv26/crate/Syntax.scala
@@ -1,11 +1,11 @@
 package org.whsv26.crate
 
 import java.sql.Timestamp
-import java.time.Instant
+import java.time.{Instant, LocalDateTime, ZoneId}
 
 object Syntax {
   implicit class StringSyntax(val s: String) {
-    def toInstant: Instant = Timestamp.valueOf(s).toInstant
+    def toInstant: Instant = Instant.from(LocalDateTime.parse(s.replace(' ', 'T')).atZone(ZoneId.of("Z")))
   }
   implicit class InstantSyntax(val i: Instant) {
     def toTimeStampString: String = Timestamp.from(i).toString
